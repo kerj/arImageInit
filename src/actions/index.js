2c46ace184b410e47a6c648c43ebf4fa8d2419f2
thekerj <kerntzj@gmail.com>
2020-04-30 16:05:52 -0700

    add state slices, actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,3 +2,31 @@ export const getPhoto = (store, image) => {
   const photo = image;
   store.setState({ photo })
 }
+
+export const setMode = (store, newMode) => {
+  const activeMode = newMode;
+  store.setState({ activeMode })
+ }
+
+ export const setStickerAttrs = (store, dims) => {
+   const stickerAttrs = dims;
+   store.setState({ stickerAttrs })
+ }
+
+ export const restart = (store) => {
+  store.setState({
+    photo: null,
+    activeMode: 'takePhoto',
+    stickerAttrs: {
+      stickerDims: {
+        width: null,
+        height: null
+      },
+      camDims: {
+        width: null,
+        height: null
+      },
+      stickerXY: [],
+    }
+  })
+ }
--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,22 @@ import * as actions from "../actions";
 
 const initialState = {
   photo: null,
+  activeMode: 'takePhoto',
+  mode: [
+    'takePhoto',
+    'viewPhoto',
+  ],
+  stickerAttrs: {
+    stickerDims: {
+      width: null,
+      height: null
+    },
+    camDims: {
+      width: null,
+      height: null
+    },
+    stickerXY: [],
+  }
 }
 
 const useGlobal = useGlobalHook(React, initialState, actions);
